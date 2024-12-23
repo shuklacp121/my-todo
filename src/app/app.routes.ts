f1e60baf9e7e836fcdcae66f8e2dddf4501b99a6
Shuklacp121 <shuklacp121@gmail.com>
2024-12-23 20:13:16 +0530

    my first change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
+import { TaskAddComponent } from './components/task-add/task-add.component';
+import { TaskViewComponent } from './components/task-view/task-view.component';
+import { title } from 'process';
+import { TaskEditComponent } from './components/task-edit/task-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        //contains the whole todo list
+        path: 'tasks',
+        component: TodoComponent
+    },
+    {
+        //display the form to add new task
+        path: 'tasks/add',
+        component: TaskAddComponent,
+        data: {titile: 'Add new task'}
+    },
+    {
+        //display the form to view a task
+        path: 'tasks/:id',
+        component: TaskViewComponent,
+        data: {title: 'Task to do'}
+    },
+    {
+        //display the form to edit a task
+        path: 'tasks/:id/edit',
+        component: TaskEditComponent,
+        data: {title: 'Task edition'},
+    },
+    {
+        path: '',
+        redirectTo: '/tasks',
+        pathMatch: 'full'
+    }
+];
---
+++ src/app/components/task-edit/task-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-edit.component.html',
+  styleUrl: './task-edit.component.css'
+})
+export class TaskEditComponent {
+
+}
